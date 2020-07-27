fa03de7d827ca9dc9e70a2a227371a92e8b07d33
priyanka borse <borasepriyanka14@gmail.com>
2020-07-27 21:05:25 +0530

    Add updated file

--- header.php
+++ header.php
@@ -16,7 +16,16 @@
     }
 </style>
 
-
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
 
 
 
@@ -44,8 +53,8 @@
 			  <a href="#contact">Blog</a>
 			  <a href="product.php">Add Product</a>
 			  <a><button type="button" class="btn btn-info">Login</button></a>
-			  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-				<i class="fa fa-bars"></i>
+			  <a href="javascript:void(0);" class="icon" onclick="myFunction()" >
+				<i class="fa fa-bars" ></i>
 			  </a>
 			</div>	
 			<hr style = "color : #000">
